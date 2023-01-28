0b91264c6c1192aa33ef4fd25026be045b2516de
naquib1229 <naquib.alam1229@gmail.com>
2023-01-28 09:13:57 +0530

    Error Handling

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var reqcount = 0;
 
-navigator.geolocation.watchPosition(successCallback, erroCallback, options);
+const watchId = navigator.geolocation.watchPosition(successCallback, erroCallback, options);
 
 function successCallback(position) {
   const {accuracy, latitude, longitude, altitude, heading, speed} = position.coords;
 //Show a map centered at latitude / longitude
 reqcount++;
+// const details = document.getElementById("details");
 details.innerHTML = "Accuracy: " +accuracy+ "<br>";
 details.innerHTML += "Latitude: " +latitude+ " | longitude " +longitude+ "<br>";
 details.innerHTML += "Altitude: " +altitude+ "<br>";
@@ -17,6 +18,22 @@ details.innerHTML += "reqcount: " +reqcount;
  
 function erroCallback(error) {
 
+  //Display error based on the error code
+  const {code} = error;
+  switch (code) {
+    case GeolocationPositionError.TIMEOUT :
+      //Handle timeout
+      alert("timeout");
+      break;
+    case GeolocationPositionError.PERMISSION_DENIED :
+      //User denied the request
+      alert("Please allow the location permission");
+      break;
+    case GeolocationPositionError.POSITION_UNAVAILABLE :
+      //Position not available
+      alert("Position not available");
+      break;
+  }
 }
 
 var options = {
@@ -24,3 +41,8 @@ var options = {
   timeout: 5000,
   maximumAge: 0
 };
+
+function buttonClickHandler() {
+  //cancel the updates when the user clicks on button
+  navigator.geolocation.clearWatch(watchId);
+}
